c0c69bb3923eb4ea0e91069e46f9cb3a689c83ec
berna <berna@DESKTOP-FBM964N>
2022-10-24 10:40:40 -0400

    localadmin manager view hotfix

--- restaurantmanager/src/main/java/edu/sru/group7/restaurantmanager/repository/ManagerRepository.java
+++ restaurantmanager/src/main/java/edu/sru/group7/restaurantmanager/repository/ManagerRepository.java
@@ -1,17 +1,20 @@
 package edu.sru.group7.restaurantmanager.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 import edu.sru.group7.restaurantmanager.domain.Managers;
 import edu.sru.group7.restaurantmanager.domain.Restaurants;
+import edu.sru.group7.restaurantmanager.domain.Servers;
 
 @Service
 public interface ManagerRepository extends CrudRepository<Managers, Long> {
 
 	@Query(value="SELECT * FROM restaurantmanagerdb.managers WHERE restaurant_id = :id", nativeQuery=true)
-	public Restaurants findByRestaurant(long id);
+	public List<Managers> findByRestaurant(long id);
 	
 }
 
