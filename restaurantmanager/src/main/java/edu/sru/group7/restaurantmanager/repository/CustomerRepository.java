af4d8f5f0dfb8eb3754c3af30c0f89cc65761e8c
berna <berna@DESKTOP-FBM964N>
2022-11-10 22:10:34 -0500

    bugfixes, removing old files. Javadoc

--- restaurantmanager/src/main/java/edu/sru/group7/restaurantmanager/repository/CustomerRepository.java
+++ restaurantmanager/src/main/java/edu/sru/group7/restaurantmanager/repository/CustomerRepository.java
@@ -21,4 +21,7 @@ public interface CustomerRepository extends CrudRepository<Customers, Long> {
 	 */
 	@Query(value="SELECT customers.* FROM ((restaurantmanagerdb.customers INNER JOIN orders ON orders.restaurant = :id AND orders.customer_id = id)", nativeQuery=true)
 	public List<Customers> findByOrderLocation(long id);
+
+	@Query(value ="SELECT * FROM restaurantmanagerdb.customers WHERE email = :email", nativeQuery=true)
+	public Customers findByEmail(String email);
 }
