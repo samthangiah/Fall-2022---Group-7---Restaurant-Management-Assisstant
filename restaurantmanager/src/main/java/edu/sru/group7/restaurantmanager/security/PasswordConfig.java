358bf6d303c194f89cf2f3ee0b17ce49df39acf5
Ethan Pasman <eap1008@sru.edu>
2022-11-20 21:54:06 -0500

    Begin Unit testing for restaurantController

--- restaurantmanager/src/main/java/edu/sru/group7/restaurantmanager/security/PasswordConfig.java
+++ restaurantmanager/src/main/java/edu/sru/group7/restaurantmanager/security/PasswordConfig.java
@@ -1,14 +1,15 @@
-
 package edu.sru.group7.restaurantmanager.security;
 
-import org.springframework.context.annotation.Bean; import
-org.springframework.context.annotation.Configuration; import
-org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; import
-org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean; 
+import org.springframework.context.annotation.Configuration; 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; 
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration public class PasswordConfig {
+@Configuration 
+public class PasswordConfig {
 
-	@Bean public PasswordEncoder passwordEncoder() { return new
-			BCryptPasswordEncoder(10); }
-
+	@Bean 
+	public PasswordEncoder passwordEncoder() { 
+		return new BCryptPasswordEncoder(10); 
+	}
 }
